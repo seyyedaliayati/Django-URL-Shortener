22ee2604420360e96f2bab6882c9082620a87d77
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2022-01-23 12:29:42 +0330

    Replaced delete fbv with cbv

--- url_shortener/app_urls/urls.py
+++ url_shortener/app_urls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     re_path(r'^$', views.NewLinkView.as_view(), name='index'),
     re_path(r'^(?P<alias>[a-zA-Z0-9-_]+)$', views.LinkRedirectView.as_view(), name='alias'),
     re_path(r'^(?P<alias>[a-zA-Z0-9-_]+)(?P<extra>/.*)$', views.LinkRedirectView.as_view(), name='alias'),
-    re_path(r'^(?P<alias>[a-zA-Z0-9-_]+)\+delete$', views.delete_link, name='delete_link'),
+    re_path(r'^(?P<alias>[a-zA-Z0-9-_]+)\+delete$', views.DeleteLinkView.as_view(), name='delete'),
     re_path(r'^(?P<alias>[a-zA-Z0-9-_]+)\+$', views.LinkPreview.as_view(), name='preview'),
     re_path(r'^~analytics/$', views.AnalyticsView.as_view(), name='analytics'),
 ]
